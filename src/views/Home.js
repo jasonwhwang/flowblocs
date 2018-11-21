5841d76b8315cfc1e50a94bf79ea8d850214aebb
Jason Hwang <jasonwhwang@gmail.com>
2018-11-21 15:29:23 -0800

    Vh Fix attempt

--- src/views/Home.js
+++ src/views/Home.js
@@ -8,7 +8,22 @@ import Schedule from './Schedule/schedule';
 import Time from './Time/time';
 
 class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.initializeVh = this.initializeVh.bind(this);
+  }
+
+  initializeVh() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    window.addEventListener('resize', () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
+
   render() {
+    this.initializeVh();
     return (
       <SwipeableViews index={2} enableMouseEvents={true}>
         <Milestones />
