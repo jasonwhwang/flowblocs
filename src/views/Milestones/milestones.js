461315640999b23a0c4ae26b2e25f379a359eed5
Jason Hwang <jasonwhwang@gmail.com>
2018-11-21 21:52:23 -0800

    Fix Milestone Overflow

--- src/views/Milestones/milestones.js
+++ src/views/Milestones/milestones.js
@@ -21,10 +21,12 @@ class Milestones extends Component {
           <button className="m-moreButton"><i className="ion-md-more m-moreicon"></i></button>
         </div>
 
-        <div className="m-swipeWrapper"><SwipeableViews containerStyle={styles.slideContainer} index={0} enableMouseEvents={true} axis="y-reverse">
-          <Milestone />
-          <Milestone />
-        </SwipeableViews></div>
+        <div className="m-swipeWrapper">
+          <SwipeableViews containerStyle={styles.slideContainer} index={0} enableMouseEvents={true} axis="y-reverse">
+            <Milestone />
+            <Milestone />
+          </SwipeableViews>
+        </div>
 
         <div className="m-arrow box-flexRowCenter">
           <i className="ion-ios-arrow-down"></i>
