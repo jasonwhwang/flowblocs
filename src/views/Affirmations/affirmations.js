c4a3116479b96eafb1a24a803f6b40451920fd77
Jason Hwang <jasonwhwang@gmail.com>
2018-11-22 14:15:04 -0800

    Textarea Autoexpand

--- src/views/Affirmations/affirmations.js
+++ src/views/Affirmations/affirmations.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
 import './affirmations.css';
 
+const autoExpand = (textarea) => {
+  var limitRows = 5;
+  var messageLastScrollHeight = textarea.scrollHeight;
+
+  textarea.oninput = function () {
+    var rows = parseInt(textarea.getAttribute("rows"));
+    // If we don't decrease the amount of rows, the scrollHeight would show the scrollHeight for all the rows
+    // even if there is no text.
+    textarea.setAttribute("rows", "1");
+
+    if (rows < limitRows && textarea.scrollHeight > messageLastScrollHeight) {
+      rows++;
+    } else if (rows > 1 && textarea.scrollHeight < messageLastScrollHeight) {
+      rows--;
+    }
+
+    messageLastScrollHeight = textarea.scrollHeight;
+    textarea.setAttribute("rows", rows);
+  };
+};
+
 class Affirmations extends Component {
+  constructor(props) {
+    super(props);
+    this.inputExpand = this.inputExpand.bind(this);
+  }
+
+  inputExpand() {
+    document.addEventListener('input', function (event) {
+      if (event.target.tagName.toLowerCase() !== 'textarea') return;
+      autoExpand(event.target);
+    }, false);
+  }
+
   render() {
+    this.inputExpand();
     return (
       <div className="affirmations">
         <div className="affirmations-page">
@@ -23,13 +57,13 @@ class Affirmations extends Component {
           </div>
 
           <div>
-            <input></input>
+            <textarea rows="1"></textarea>
           </div>
 
           <div className="a-filler"></div>
-          
+
           <div>
-            <input></input>
+            <textarea rows="1"></textarea>
           </div>
 
         </div>
